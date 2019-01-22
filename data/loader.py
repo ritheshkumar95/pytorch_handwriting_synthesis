1efb4d613976773bb67fadbed561ec39bba3d45f
Rithesh Kumar <rithesh@lyrebird.ai>
2019-01-22 21:20:54 +0000

    fixing data processing code

--- data/loader.py
+++ data/loader.py
@@ -1,67 +1,39 @@
-import glob
-from pathlib import Path
-from xml.etree.ElementTree import ElementTree
-import numpy as np
-from utils import align, denoise, normalize, coords_to_offsets, draw
+import h5py
+import torch
 from tqdm import tqdm
-import pickle
+from torch.utils.data import DataLoader
 
 
-class DataLoader(object):
-    def __init__(self, data_path, max_stroke_len):
-        self.path = Path(data_path)
-        self.max_stroke_len = max_stroke_len
+class HandwritingSynthDataset(torch.utils.data.Dataset):
+    def __init__(
+        self, hdf5_path
+    ):
+        self.h5 = h5py.File(hdf5_path, 'r')
+        self.vocab = eval(self.h5.attrs['vocab'])
+        self.char2idx = eval(self.h5.attrs['char2idx'])
 
-        if (self.path / 'processed.pkl').exists():
-            self.sentences, self.lineStrokes = pickle.load(
-                open(self.path / 'processed.pkl', 'rb')
-            )
-        else:
-            self.sentences, self.lineStrokes = self.process_data()
-            pickle.dump(
-                [self.sentences, self.lineStrokes],
-                open(self.path / 'processed.pkl', 'wb')
-            )
+    def __getitem__(self, index):
+        chars = self.h5['chars'][index]
+        chars_mask = self.h5['chars_mask'][index]
+        strokes = self.h5['strokes'][index]
+        strokes_mask = self.h5['strokes_mask'][index]
 
-    def process_data(self):
-        files = glob.glob(str(self.path / "ascii/**/*.txt"), recursive=True)[:100]
-        sentences = []
-        lineStrokes = []
+        chars, chars_mask, strokes, strokes_mask = [
+            torch.from_numpy(x)
+            for x in [chars, chars_mask, strokes, strokes_mask]
+        ]
 
-        for i, file in tqdm(enumerate(files)):
-            root = Path(file)
-            data = open(root).read().splitlines()
-            data = [x.strip() for x in data]
-            start_idx = data.index('CSR:') + 2
-            text = data[start_idx:]
-            sentences += text
+        return chars, chars_mask, strokes, strokes_mask
 
-            try:
-                for j in range(1, 1 + len(text)):
-                    stroke_path = str(root).replace('ascii', 'lineStrokes')
-                    stroke_path = stroke_path.replace('.txt', '-%02d.xml' % j)
-                    et = ElementTree().parse(stroke_path)
-                    arr = []
-                    for stroke in et[1]:  # loop through Strokes in a StrokeSet
-                        for point in stroke:  # loop through points in a Stroke
-                            arr.append([int(point.attrib['x']), int(point.attrib['y']), 0])
-                        arr[-1][-1] = 1  # Stroke ends, pen up!
-
-                    coords = np.array(arr)
-                    coords = align(coords)
-                    coords = denoise(coords)
-                    offsets = coords_to_offsets(coords)[:self.max_stroke_len]
-                    offsets = normalize(offsets)
-                    lineStrokes.append(offsets)
-            except FileNotFoundError:
-                for j in range(len(text)):
-                    sentences.pop()
-
-        return sentences, lineStrokes
+    def __len__(self):
+        return len(self.h5['sentences'])
 
 
 if __name__ == '__main__':
-    loader = DataLoader('/Users/rithesh/iam', 1200)
-    for i in range(100):
-        draw(loader.lineStrokes[i], loader.sentences[i])
-        input()
+    path = '/data/pytorch_handwriting_synthesis/data.hdf5'
+    dataset = HandwritingSynthDataset(path)
+    loader = DataLoader(dataset, batch_size=64, num_workers=2)
+    for i, data in tqdm(enumerate(loader)):
+        if i == 0:
+            for x in data:
+                print(x.shape)
--- data/process_raw_data.py
+++ data/process_raw_data.py
@@ -3,13 +3,21 @@ import glob
 from pathlib import Path
 from xml.etree.ElementTree import ElementTree
 import numpy as np
-from utils import align, denoise, normalize, coords_to_offsets, draw
+from utils import align, denoise, normalize, coords_to_offsets
+from multiprocessing import Pool
 from tqdm import tqdm
 import h5py
 
 
-def get_strokes(filename, max_stroke_len):
-    et = ElementTree().parse(stroke_path)
+def get_sentences(filename):
+    data = open(filename).read().splitlines()
+    data = [x.strip() for x in data]
+    start_idx = data.index('CSR:') + 2
+    return [list(x) for x in data[start_idx:]]
+
+
+def get_strokes(filename):
+    et = ElementTree().parse(filename)
     arr = []
     for stroke in et[1]:  # loop through Strokes in a StrokeSet
         for point in stroke:  # loop through points in a Stroke
@@ -23,7 +31,7 @@ def get_strokes(filename, max_stroke_len):
     coords = np.array(arr)
     coords = align(coords)
     coords = denoise(coords)
-    offsets = coords_to_offsets(coords)[:max_stroke_len]
+    offsets = coords_to_offsets(coords)[:args.max_stroke_len]
     offsets = normalize(offsets)
     return offsets
 
@@ -31,57 +39,95 @@ def get_strokes(filename, max_stroke_len):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_file', default='data.hdf5')
-    parser.add_argument("--data_path", default='/Users/rithesh/iam')
+    parser.add_argument("--data_path", default='/data/iam_ondb')
     parser.add_argument("--max_stroke_len", type=int, default=1200)
     args = parser.parse_args()
     return args
 
 
-args = parse_args()
-f = h5py.File(args.save_file, 'w')
-files = glob.glob(
-    args.data_path + "/ascii/**/*.txt",
-    recursive=True
-)[:100]
+if __name__ == '__main__':
+    args = parse_args()
+    f = h5py.File(args.save_file, 'w')
+    files = glob.glob(
+        args.data_path + "/ascii/**/*.txt",
+        recursive=True
+    )
 
-sentences = []
-strokePaths = []
+    #################################################################
+    # Extracting sentences and corresponding stroke paths           #
+    #################################################################
+    sentences = []
+    strokePaths = []
 
-for i, file in tqdm(enumerate(files)):
-    root = Path(file)
+    for i, file in tqdm(enumerate(files)):
+        root = Path(file)
+        text = get_sentences(root)
 
-    stroke_root = str(root).replace('ascii', 'lineStrokes')
-    if not Path(stroke_root).parent.exists():
-        print("Skipping %s" % str(root))
-        continue
+        stroke_root = str(root).replace('ascii', 'lineStrokes')
+        # Bugs in database unfortunately :(
+        if not Path(stroke_root).parent.exists():
+            print("Skipping %s" % str(root))
+            continue
 
-    data = open(root).read().splitlines()
-    data = [x.strip() for x in data]
-    start_idx = data.index('CSR:') + 2
-    text = data[start_idx:]
-    sentences += [np.string_(x) for x in text]
+        for j, line in enumerate(text):
+            stroke_path = str(root).replace('ascii', 'lineStrokes')
+            stroke_path = stroke_path.replace('.txt', '-%02d.xml' % j)
 
-    for j in range(1, 1 + len(text)):
-        stroke_path = str(root).replace('ascii', 'lineStrokes')
-        stroke_path = stroke_path.replace('.txt', '-%02d.xml' % j)
-        strokePaths.append(stroke_path)
+            # Bugs in database unfortunately :(
+            if not Path(stroke_path).exists():
+                print("Skipping %s" % stroke_path)
+                continue
 
+            strokePaths.append(stroke_path)
+            sentences += [line]
 
-# print(sentences)
-total_string = np.string_('')
-for sent in sentences:
-    total_string += sent
+    ################################################
+    # Create vocabulary, char2idx                  #
+    ################################################
+    total_string = ['']
+    for sent in sentences:
+        total_string += sent
 
-print(total_string)
-import ipdb; ipdb.set_trace()
-vocab = list(set(total_string))
-word2idx = {x: i for i, x in enumerate(vocab)}
+    vocab = sorted(list(set(total_string)))
+    char2idx = {x: i for i, x in enumerate(vocab)}
 
-idxs = [[word2idx[char] for char in line] for line in sentences]
-chars = [''.join([vocab[i] for i in line]) for line in idxs]
+    f.attrs['vocab'] = str(vocab)
+    f.attrs['char2idx'] = str(char2idx)
 
-f.attrs['vocab'] = str(vocab)
-f.attrs['char2idx'] = str(word2idx)
+    print("Created vocabulary...")
 
-f.create_dataset('sentences', data=sentences, dtype=h5py.special_dtype(vlen=str))
-f.close()
+    ################################################
+    # Convert sentences to indices                 #
+    ################################################
+    idx_sentences = [[char2idx[x] for x in sent] for sent in sentences]
+    lengths = [len(x) for x in idx_sentences]
+    max_char_len = int(np.percentile(lengths, 95))
+
+    chars_arr = np.zeros((len(sentences), max_char_len), dtype='int64')
+    chars_mask_arr = np.zeros((len(sentences), max_char_len), dtype='float32')
+
+    for i, sent in enumerate(idx_sentences):
+        length = min(len(sent), max_char_len)
+        chars_arr[i, :length] = sent[:length]
+        chars_mask_arr[i, :length] = 1.
+
+    f.create_dataset('sentences', data=sentences, dtype=h5py.special_dtype(vlen=str))
+    f['chars'] = chars_arr
+    f['chars_mask'] = chars_mask_arr
+
+    ################################################
+    # Extract strokes from strokePaths             #
+    ################################################
+    pool = Pool()
+    strokes = pool.map(get_strokes, strokePaths)
+    # strokes = [get_strokes(path) for path in strokePaths]
+    strokes_arr = np.zeros((len(strokes), args.max_stroke_len, 3), dtype='float32')
+    strokes_mask_arr = np.zeros((len(strokes), args.max_stroke_len), dtype='float32')
+
+    for i, stk in enumerate(strokes):
+        strokes_arr[i, :stk.shape[0]] = stk
+        strokes_mask_arr[i, :stk.shape[0]] = 1.
+
+    f['strokes'] = strokes_arr
+    f['strokes_mask'] = strokes_mask_arr
+    f.close()
