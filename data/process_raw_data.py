623ba464e0dfd0cdaa5cc80d989e6ecc4b2a3e7e
Rithesh Kumar <rithesh@lyrebird.ai>
2019-01-28 05:09:08 +0000

    switching data preproc

--- data/process_raw_data.py
+++ data/process_raw_data.py
@@ -1,12 +1,8 @@
 import argparse
-import glob
-from pathlib import Path
-from xml.etree.ElementTree import ElementTree
+import os
+from xml.etree import ElementTree
 import numpy as np
-from utils import align, denoise, normalize, coords_to_offsets
-from multiprocessing import Pool
-from tqdm import tqdm
-import h5py
+import utils
 
 
 def parse_args():
@@ -18,118 +14,132 @@ def parse_args():
     return args
 
 
-def get_sentences(filename):
-    data = open(filename).read().splitlines()
-    data = [x.strip() for x in data]
-    start_idx = data.index('CSR:') + 2
-    return [list(x) for x in data[start_idx:]]
+def get_stroke_sequence(filename):
+    tree = ElementTree.parse(filename).getroot()
+    strokes = [i for i in tree if i.tag == 'StrokeSet'][0]
 
-
-def get_strokes(filename):
-    et = ElementTree().parse(filename)
-    arr = []
-    for stroke in et[1]:  # loop through Strokes in a StrokeSet
-        for point in stroke:  # loop through points in a Stroke
-            arr.append([
+    coords = []
+    for stroke in strokes:
+        for i, point in enumerate(stroke):
+            coords.append([
                 int(point.attrib['x']),
                 int(point.attrib['y']),
-                0
+                int(i == len(stroke) - 1)
             ])
-        arr[-1][-1] = 1  # Stroke ends, pen up!
+    coords = np.array(coords)
 
-    coords = np.array(arr)
-    coords = align(coords)
-    coords = denoise(coords)
-    offsets = coords_to_offsets(coords)[:args.max_stroke_len]
-    offsets = normalize(offsets)
+    coords = utils.align(coords)
+    coords = utils.denoise(coords)
+    offsets = utils.coords_to_offsets(coords)
+    offsets = offsets[:utils.MAX_STROKE_LEN]
+    offsets = utils.normalize(offsets)
     return offsets
 
-if __name__ == '__main__':
-    args = parse_args()
-    f = h5py.File(args.save_file, 'w')
-    files = glob.glob(
-        args.data_path + "/ascii/**/*.txt",
-        recursive=True
-    )
 
-    #################################################################
-    # Extracting sentences and corresponding stroke paths           #
-    #################################################################
-    sentences = []
-    strokePaths = []
+def get_ascii_sequences(filename):
+    sequences = open(filename, 'r').read()
+    sequences = sequences.replace(r'%%%%%%%%%%%', '\n')
+    sequences = [i.strip() for i in sequences.split('\n')]
+    lines = sequences[sequences.index('CSR:') + 2:]
+    lines = [line.strip() for line in lines if line.strip()]
+    lines = [utils.encode_ascii(line)[:utils.MAX_CHAR_LEN] for line in lines]
+    return lines
 
-    for i, file in tqdm(enumerate(files)):
-        root = Path(file)
-        text = get_sentences(root)
 
-        stroke_root = str(root).replace('ascii', 'lineStrokes')
-        # Bugs in database unfortunately :(
-        if not Path(stroke_root).parent.exists():
-            print("Skipping %s" % str(root))
+def collect_data():
+    fnames = []
+    for dirpath, dirnames, filenames in os.walk('data/raw/ascii/'):
+        if dirnames:
+            continue
+        for filename in filenames:
+            if filename.startswith('.'):
+                continue
+            fnames.append(os.path.join(dirpath, filename))
+
+    # low quality samples (selected by collecting samples to
+    # which the trained model assigned very low likelihood)
+    blacklist = set(np.load('data/blacklist.npy'))
+
+    stroke_fnames, transcriptions, writer_ids = [], [], []
+    for i, fname in enumerate(fnames):
+        print(i, fname)
+        if fname == 'data/raw/ascii/z01/z01-000/z01-000z.txt':
             continue
 
-        for j, line in enumerate(text):
-            stroke_path = str(root).replace('ascii', 'lineStrokes')
-            stroke_path = stroke_path.replace('.txt', '-%02d.xml' % j)
+        head, tail = os.path.split(fname)
+        last_letter = os.path.splitext(fname)[0][-1]
+        last_letter = last_letter if last_letter.isalpha() else ''
 
-            # Bugs in database unfortunately :(
-            if not Path(stroke_path).exists():
-                print("Skipping %s" % stroke_path)
+        line_stroke_dir = head.replace('ascii', 'lineStrokes')
+        line_stroke_fname_prefix = os.path.split(head)[-1] + last_letter + '-'
+
+        if not os.path.isdir(line_stroke_dir):
+            continue
+        line_stroke_fnames = sorted([f for f in os.listdir(line_stroke_dir)
+                                     if f.startswith(line_stroke_fname_prefix)])
+        if not line_stroke_fnames:
+            continue
+
+        original_dir = head.replace('ascii', 'original')
+        original_xml = os.path.join(original_dir, 'strokes' + last_letter + '.xml')
+        tree = ElementTree.parse(original_xml)
+        root = tree.getroot()
+
+        general = root.find('General')
+        if general is not None:
+            writer_id = int(general[0].attrib.get('writerID', '0'))
+        else:
+            writer_id = int('0')
+
+        ascii_sequences = get_ascii_sequences(fname)
+        assert len(ascii_sequences) == len(line_stroke_fnames)
+
+        for ascii_seq, line_stroke_fname in zip(ascii_sequences, line_stroke_fnames):
+            if line_stroke_fname in blacklist:
                 continue
 
-            strokePaths.append(stroke_path)
-            sentences += [line]
+            stroke_fnames.append(os.path.join(line_stroke_dir, line_stroke_fname))
+            transcriptions.append(ascii_seq)
+            writer_ids.append(writer_id)
 
-    ################################################
-    # Create vocabulary, char2idx                  #
-    ################################################
-    total_string = ['']
-    for sent in sentences:
-        total_string += sent
+    return stroke_fnames, transcriptions, writer_ids
 
-    vocab = sorted(list(set(total_string)))
-    char2idx = {x: i for i, x in enumerate(vocab)}
 
-    f.attrs['vocab'] = str(vocab)
-    f.attrs['char2idx'] = str(char2idx)
+if __name__ == '__main__':
+    print('downloading blacklist file...')
+    os.system(
+        'wget https://github.com/sjvasquez/handwriting-synthesis/raw/master/data/blacklist.npy -P ./data'
+    )
+    print('traversing data directory...')
+    stroke_fnames, transcriptions, writer_ids = collect_data()
 
-    print("Created vocabulary...")
+    print('dumping to numpy arrays...')
+    x = np.zeros([len(stroke_fnames), utils.MAX_STROKE_LEN, 3], dtype=np.float32)
+    x_len = np.zeros([len(stroke_fnames)], dtype=np.int16)
+    c = np.zeros([len(stroke_fnames), utils.MAX_CHAR_LEN], dtype=np.int8)
+    c_len = np.zeros([len(stroke_fnames)], dtype=np.int8)
+    w_id = np.zeros([len(stroke_fnames)], dtype=np.int16)
+    valid_mask = np.zeros([len(stroke_fnames)], dtype=np.bool)
 
-    ################################################
-    # Convert sentences to indices                 #
-    ################################################
-    idx_sentences = [[char2idx[x] for x in sent] for sent in sentences]
-    lengths = [len(x) for x in idx_sentences]
-    max_char_len = int(np.percentile(lengths, 95))
+    for i, (stroke_fname, c_i, w_id_i) in enumerate(zip(stroke_fnames, transcriptions, writer_ids)):
+        if i % 200 == 0:
+            print(i, '\t', '/', len(stroke_fnames))
+        x_i = get_stroke_sequence(stroke_fname)
+        valid_mask[i] = ~np.any(np.linalg.norm(x_i[:, :2], axis=1) > 60)
 
-    chars_arr = np.zeros((len(sentences), max_char_len), dtype='int64')
-    chars_mask_arr = np.zeros((len(sentences), max_char_len), dtype='float32')
+        x[i, :len(x_i), :] = x_i
+        x_len[i] = len(x_i)
 
-    for i, sent in enumerate(idx_sentences):
-        length = min(len(sent), max_char_len)
-        chars_arr[i, :length] = sent[:length]
-        chars_mask_arr[i, :length] = 1.
+        c[i, :len(c_i)] = c_i
+        c_len[i] = len(c_i)
 
-    f.create_dataset('sentences', data=sentences, dtype=h5py.special_dtype(vlen=str))
-    f['chars'] = chars_arr
-    f['chars_mask'] = chars_mask_arr
+        w_id[i] = w_id_i
 
-    ################################################
-    # Extract strokes from strokePaths             #
-    ################################################
-    pool = Pool()
-    strokes = pool.map(get_strokes, strokePaths)
+    if not os.path.isdir('data/processed'):
+        os.makedirs('data/processed')
 
-    import ipdb; ipdb.set_trace()
-
-    # strokes = [get_strokes(path) for path in strokePaths]
-    strokes_arr = np.zeros((len(strokes), args.max_stroke_len, 3), dtype='float32')
-    strokes_mask_arr = np.zeros((len(strokes), args.max_stroke_len), dtype='float32')
-
-    for i, stk in enumerate(strokes):
-        strokes_arr[i, :stk.shape[0]] = stk
-        strokes_mask_arr[i, :stk.shape[0]] = 1.
-
-    f['strokes'] = strokes_arr
-    f['strokes_mask'] = strokes_mask_arr
-    f.close()
+    np.save('data/processed/x.npy', x[valid_mask])
+    np.save('data/processed/x_len.npy', x_len[valid_mask])
+    np.save('data/processed/c.npy', c[valid_mask])
+    np.save('data/processed/c_len.npy', c_len[valid_mask])
+    np.save('data/processed/w_id.npy', w_id[valid_mask])
--- data/utils.py
+++ data/utils.py
@@ -1,6 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import savgol_filter
+from collections import defaultdict
+
+
+alphabet = [
+    '\x00', ' ', '!', '"', '#', "'", '(', ')', ',', '-', '.',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';',
+    '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+    'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'Y',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z'
+]
+alphabet_ord = list(map(ord, alphabet))
+alpha_to_num = defaultdict(int, list(map(reversed, enumerate(alphabet))))
+num_to_alpha = dict(enumerate(alphabet_ord))
+
+MAX_STROKE_LEN = 1200
+MAX_CHAR_LEN = 75
 
 
 def align(coords):
@@ -61,6 +79,13 @@ def offsets_to_coords(offsets):
     convert from offsets to coordinates
     """
     return np.concatenate([np.cumsum(offsets[:, :2], axis=0), offsets[:, 2:3]], axis=1)
+
+
+def encode_ascii(ascii_string):
+    """
+    encodes ascii string to array of ints
+    """
+    return np.array(list(map(lambda x: alpha_to_num[x], ascii_string)) + [0])
 
 
 def draw(
